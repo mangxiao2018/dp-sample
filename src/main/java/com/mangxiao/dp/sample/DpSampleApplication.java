d963c684c8b011604108e3f619c5f87f8070a3ea
mangxiao <mangxiao2018@126.com>
2020-08-10 18:13:47 +0800

    设计模式工程初创

---
+++ src/main/java/com/mangxiao/dp/sample/DpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mangxiao.dp.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DpSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mangxiao/dp/sample/DpSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mangxiao.dp.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
